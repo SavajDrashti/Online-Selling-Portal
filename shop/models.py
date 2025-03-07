8be8d610cb426bfc91e191e60dc036e7c1fbee69
SavajDrashti <savajdrashti2511@gmail.com>
2025-03-07 15:24:36 +0530

    change in model

--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='categories/', blank=True, null=True)
+    image = CloudinaryField('category_image', blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -15,7 +16,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    image = CloudinaryField('product_image')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     stock = models.PositiveIntegerField(default=0)
 
