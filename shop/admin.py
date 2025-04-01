272aad00b11757f6def41e137b77f1d5772783a8
SavajDrashti <155703727+SavajDrashti@users.noreply.github.com>
2025-04-01 15:28:53 +0530

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -62,13 +62,13 @@ class ReviewAdmin(admin.ModelAdmin):
     readonly_fields = [field.name for field in Review._meta.fields]
 
     def has_add_permission(self, request):
-        return False
+        return True
     
     def has_change_permission(self, request, obj = None):
-        return False
+        return True
     
     def has_delete_permission(self, request, obj = None):
-        return False
+        return True
     
 class WishlistAdmin(admin.ModelAdmin):
 
